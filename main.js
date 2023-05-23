4eebba1def4b790043029136d169861ee9565e12
Ruslan Symonenko <symonenkors@gmail.com>
2023-05-23 17:38:27 +0300

    finished the app functionality

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const tipPercentBtns = document.querySelectorAll('.app-controllers__tip-item');
+  const totalTipAmountContainer = document.querySelector('#tip-amount');
+  const totalTipForPerson = document.querySelector('#tip-person');
+  const calculateBtn = document.querySelector('.btn-calculate');
+  const resetBtn = document.querySelector('.btn-reset');
+
+  const billInput = document.querySelector('.app-controllers__bill-input');
+  const peopleInput= document.querySelector('.app-controllers__people-input');
 
   let bill = 0;
   let numberOfPeople = 1;
@@ -7,11 +14,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   function setActiveTip (btn) {
-    if(btn.textContent != 'Custom') {
-      let btnAmount = parseInt(btn.textContent, 10);
-      activeTipPercent = btnAmount;
-    } else {
-      console.log('custom');
+    let btnAmount = parseInt(btn.textContent, 10);
+    activeTipPercent = btnAmount;
+  }
+
+  function getInputsInfo () {
+    const billInputValue = document.querySelector('.app-controllers__bill-input').value;
+    const peopleInputValue = document.querySelector('.app-controllers__people-input').value;
+
+    if(billInputValue) {
+      bill = billInputValue;
+    }
+
+    if (peopleInputValue) {
+      numberOfPeople = peopleInputValue
     }
   }
 
@@ -25,12 +41,32 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  function calculate (tipPercent) {
+  function calculate () {
+    getInputsInfo();
+    let tipSum = bill * (activeTipPercent/100);
+    let tipForPerson = tipSum / numberOfPeople;
 
+    totalTipAmountContainer.textContent = tipSum.toFixed(2);
+    totalTipForPerson.textContent = tipForPerson.toFixed(2);
   }
 
   function reset () {
+    bill = 0;
+    numberOfPeople = 1;
+    activeTipPercent = 10;
 
+    tipPercentBtns.forEach(item => {
+      item.classList.remove('app-controllers__tip-item--active');
+      if(item.textContent === '10%') {
+        item.classList.add('app-controllers__tip-item--active');
+      }
+    })
+
+    totalTipAmountContainer.textContent = '0.00';
+    totalTipForPerson.textContent = '0.00';
+
+    billInput.value = '';
+    peopleInput.value = '';
   }
 
   tipPercentBtns.forEach(btn => {
@@ -38,6 +74,14 @@ document.addEventListener('DOMContentLoaded', () => {
       setActiveTip(btn);
       showActiveTip(tipPercentBtns, btn);
     })
+  });
+
+  calculateBtn.addEventListener('click', () => {
+    calculate();
+  });
+
+  resetBtn.addEventListener('click', () => {
+    reset();
   })
 
 })
